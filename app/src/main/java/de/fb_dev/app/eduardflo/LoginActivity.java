f8afd96b6973683bfefa2b0fd8f83bdc8b51839b
Florian Breuer <f.breuer@a3plus.de>
2016-11-24 13:52:46 +0100

    change activity sequence and edit layout

--- app/src/main/java/de/fb_dev/app/eduardflo/LoginActivity.java
+++ app/src/main/java/de/fb_dev/app/eduardflo/LoginActivity.java
@@ -3,6 +3,7 @@ package de.fb_dev.app.eduardflo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "flo@edu.com:hello"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -306,8 +307,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
@@ -315,16 +314,22 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
+            // If authentication is successfull switch activity
+            // TODO: set up Login-Funktion with Network
+
+
+
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
+                    // Account exists, return true and go to NavAction.
+                    startActivity(new Intent(LoginActivity.this, NavAction.class));
                     return pieces[1].equals(mPassword);
                 }
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
