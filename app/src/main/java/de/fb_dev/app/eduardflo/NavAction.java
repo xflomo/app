a3e302440ef15368a9d73cb7298eb4059c062228
Florian Breuer <f.breuer@a3plus.de>
2016-11-24 21:15:15 +0100

    add notification

--- app/src/main/java/de/fb_dev/app/eduardflo/NavAction.java
+++ app/src/main/java/de/fb_dev/app/eduardflo/NavAction.java
@@ -1,9 +1,14 @@
 package de.fb_dev.app.eduardflo;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -86,7 +91,33 @@ public class NavAction extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
             startActivity(new Intent(NavAction.this, LoginActivity.class));
         } else if (id == R.id.nav_slideshow) {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.ic_menu_share)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(this, NavAction.class);
 
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(NavAction.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(2, mBuilder.build());
         } else if (id == R.id.nav_manage) {
             startActivity(new Intent(NavAction.this, SettingsActivity.class));
         } else if (id == R.id.nav_share) {
@@ -98,5 +129,10 @@ public class NavAction extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onPause(){
+
     }
 }
